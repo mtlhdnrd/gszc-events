dc0fc55def200a9e43322eebb75687f9ab0f2859
mtlhdnrd <mtlhdnrd@protonmail.com>
2025-03-08 12:39:47 +0100

    get, add events

--- backend/api/events/get_events.php
+++ backend/api/events/get_events.php
@@ -1,13 +1,12 @@
 <?php
-    require_once "../config.php";
-    require_once "../api_utils.php";
-    require_once "../event.php";
+    require_once $_SERVER["DOCUMENT_ROOT"]."/bgszc-events/backend/config.php";
+    require_once $_SERVER["DOCUMENT_ROOT"]."/bgszc-events/backend/api_utils.php";
+    require_once $_SERVER["DOCUMENT_ROOT"]."/bgszc-events/backend/api/events/event.php";
     if(validate_request("GET", array())) {
         header("Content-Type: application/json");
         $query = "SELECT `event_id`, `name`, `date`, `location`, `status`, `busyness` FROM `events`;";
         $stmt = $conn->prepare($query);
         if($stmt->execute()) {
-            header("Content-Type: application/json");
             echo json_encode($stmt->get_result()->fetch_all(MYSQLI_ASSOC));
         } else {
             echo $stmt->error;
